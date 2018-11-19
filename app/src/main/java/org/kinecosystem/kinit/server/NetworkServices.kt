286ff1a529aef6a22a69513ec1e971910f653749
Shay Baz <shay@rounds.com>
2018-11-19 14:06:33 +0200

    fix_get_categories_after_submit

--- app/src/main/java/org/kinecosystem/kinit/server/NetworkServices.kt
+++ app/src/main/java/org/kinecosystem/kinit/server/NetworkServices.kt
@@ -61,9 +61,9 @@ class NetworkServices {
 
         walletService = Wallet(context.applicationContext, dataStoreProvider, userRepo, categoryRepository, analytics,
                 onboardingApi, walletApi, scheduler)
-        taskService = TaskService(applicationContext,
-                retrofit.create<TasksApi>(TasksApi::class.java), categoryRepository, userRepo, walletService)
         categoriesService = CategoriesService(applicationContext, retrofit.create(CategoriesApi::class.java), userRepo, categoryRepository)
+        taskService = TaskService(applicationContext,
+                retrofit.create<TasksApi>(TasksApi::class.java), categoryRepository, userRepo, walletService, categoriesService)
         onBoardingService = OnboardingService(applicationContext,
                 retrofit.create<OnboardingApi>(OnboardingApi::class.java),
                 retrofit.create<PhoneAuthenticationApi>(PhoneAuthenticationApi::class.java),
