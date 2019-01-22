43f838e10cb303ed3987f491110390ae05d3ead3
Shay Baz <shay@rounds.com>
2019-01-22 09:58:50 +0200

    fix_touch_area_error_screen

--- app/src/main/java/org/kinecosystem/kinit/view/transfer/TransferFailedFragment.kt
+++ app/src/main/java/org/kinecosystem/kinit/view/transfer/TransferFailedFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import org.kinecosystem.kinit.R
 import org.kinecosystem.kinit.view.BaseFragment
 
@@ -15,7 +14,7 @@ class TransferFailedFragment : BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.transferring_to_app_failed_layout, container, false)
-        view.findViewById<TextView>(R.id.close_text).setOnClickListener {
+        view.setOnClickListener {
             if (activity is TransferActions) {
                 (activity as TransferActions).onClose()
             }
