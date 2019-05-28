6945fadbabdf9a215f7eb12ab07d5e1e69d36012
Berry Ventura Lev <berry.ventura@kin.org>
2019-05-28 14:41:31 +0300

    Use move kin instead of ecosdk (#250)
    * Use move-kin-android transfer module instead of the ecosystem sdk one.
    * Upgrade picasso / kin sdk / migration / move kin versions

--- app/src/main/java/org/kinecosystem/kinit/view/transfer/AccountInfoActivity.kt
+++ app/src/main/java/org/kinecosystem/kinit/view/transfer/AccountInfoActivity.kt
@@ -1,61 +1,26 @@
 package org.kinecosystem.kinit.view.transfer
 
 
-import android.content.Context
-import android.content.Intent
-import android.databinding.DataBindingUtil.setContentView
-import android.databinding.Observable
-import android.databinding.ObservableBoolean
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.kin.ecosystem.transfer.AccountInfoManager
-import org.kinecosystem.kinit.R
 import org.kinecosystem.kinit.viewmodel.AccountInfoViewModel
+import org.kinecosystem.transfer.receiver.view.AccountInfoActivityBase
 
-class AccountInfoActivity : AppCompatActivity() {
+class AccountInfoActivity : AccountInfoActivityBase() {
 
-    lateinit var model: AccountInfoViewModel
-
-    companion object {
-        fun getIntent(context: Context): Intent {
-            return Intent(context, AccountInfoActivity::class.java)
-        }
-    }
+    private var viewModel: AccountInfoViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = setContentView<org.kinecosystem.kinit.databinding.KinecosystemActivityAccountInfoBinding>(this, R.layout.kinecosystem_activity_account_info)
-        model = AccountInfoViewModel(AccountInfoManager(this), intent)
-
-        model.onClose.addOnPropertyChangedCallback(
-                object : Observable.OnPropertyChangedCallback() {
-                    override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
-                        if (sender is ObservableBoolean && sender.get()) {
-                            finish()
-                        }
-                    }
-                })
-        binding.model = model
+        viewModel = AccountInfoViewModel()
     }
 
-    override fun onResume() {
-        super.onResume()
-        model.onResume()
+    override fun getData(): String? {
+        return viewModel?.getPublicAddress()
     }
 
-    override fun onPause() {
-        super.onPause()
-        model.onPause()
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        model.onBackPressed()
-    }
-
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        model.onDetach()
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel = null
     }
 
 }
